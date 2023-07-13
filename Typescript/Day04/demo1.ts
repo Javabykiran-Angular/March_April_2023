f4af7d395905846eb39dad28795241fcbb3245bc
The Kiran Academy <71135924+Javabykiran-Angular@users.noreply.github.com>
2023-07-13 15:56:08 +0530

    Day04

---
+++ Typescript/Day04/demo1.ts
@@ -0,0 +1,64 @@
+
+// console.log("Hello World");
+/*
+console.log('Welcome to Angular 12 Module...');
+console.log(`Hopes so u r enjoying This Module...`);
+*/
+
+// var a1,a2;
+// var a_1,a$1;
+
+//data type 
+// number(int,float,double,long,short),string(''/""),boolean(true/false)
+//any (number,string,boolean)
+//void => it does not point anything => it is a neutral data 
+// misec null & undefined => it used as value as well as data type 
+
+var a:number=10;  // Forward declaration & defination
+
+// console.log("Value of a "+a);
+// console.log('Value of a is ',a);
+
+var str!:string;
+// console.log('value of str '+str);
+str='Sumit Raokhande';
+// console.log(`Value of str is ${str} `);
+
+var mystatus:boolean;
+mystatus=true;
+// console.log(mystatus);
+
+var myany:any;
+myany=2.5;
+// console.log("value of myany is "+myany);
+myany='Sumit';
+// console.log("value of myany is "+myany);
+
+myany=false;
+// console.log("value of myany is "+myany);
+
+// literal
+
+var a1:number | string;
+
+a1=25;
+// console.log("Value of a1 is "+a1);
+a1='Sumit';
+// console.log("Value of a1 is "+a1);
+
+var a2:50 | string;
+
+a2=50;
+// console.log("value of a2 is "+a2);
+a2='Sumit';
+// console.log("Value of a2 is "+a2);
+
+var a3!:string | null;
+console.log(a3);
+
+
+
+
+
+
+
