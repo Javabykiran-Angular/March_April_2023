423bbd94060a80b9441b2b4579558f0d98adbc50
The Kiran Academy <71135924+Javabykiran-Angular@users.noreply.github.com>
2023-09-15 15:38:12 +0530

    Day30

---
+++ Angular/Day30/app-routing.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { AuthGuard } from './auth.guard';
+import { DetailsComponent } from './details/details.component';
+import { AddemployeeComponent } from './addemployee/addemployee.component';
+
+const routes: Routes = [
+    {
+        path:'',pathMatch:"full",redirectTo:'/login'
+    },
+    {
+      path:"login",component:LoginComponent
+    },
+    {
+      path:"home",component:HomeComponent,canActivate:[AuthGuard]
+    },
+    {
+      path:'details/:id',component:DetailsComponent,canActivate:[AuthGuard]
+    },
+    {
+      path:'addemp',component:AddemployeeComponent,canActivate:[AuthGuard]
+    },
+    {
+      path:"**",component:NotfoundComponent
+    }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
